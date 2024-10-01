e8f23861528ce2bffc9db712c1c4f45250928131
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-10-01 01:18:37 -0400

    separa css dos menus em próprio arquivo. coloca prefixo nas classes dos menus

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,11 @@
 function showMenu() {
-  document.getElementById('menu-retratil').classList.remove("menu-hide");
-  document.getElementById('menu-retratil').classList.add("menu-show");
+  document.getElementById('silo-menu-retratil').classList.remove("menu-hide");
+  document.getElementById('silo-menu-retratil').classList.add("menu-show");
 }
 
 function hideMenu() {
-  document.getElementById('menu-retratil').classList.remove("menu-show");
-  document.getElementById('menu-retratil').classList.add("menu-hide");
+  document.getElementById('silo-menu-retratil').classList.remove("menu-show");
+  document.getElementById('silo-menu-retratil').classList.add("menu-hide");
 }
 
 document.getElementById('show-menu').onclick = showMenu;
